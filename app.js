cf50f69db4c800be71019fc9d398a3bd02d1ac48
Tekla <davidannatekla@gmail.com>
2021-07-14 19:42:56 +0200

    test

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 
 // Set the date we're counting down to
-var countDownDate = new Date("Jul 14, 2021 19:43:00").getTime();
+var countDownDate = new Date("Jul 14, 2021 19:50:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
